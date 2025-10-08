12b3a2cbe542887901896ffcd89819ed5b47df5d
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-10-08 16:19:16 -0300

    modified line

--- src/orders/orders.routes.ts
+++ src/orders/orders.routes.ts
@@ -6,9 +6,9 @@ const router = Router();
 const ordersController = new OrdersController();
 
 router.post('/', ordersController.createOrder); //crear un pedido
+router.get('/status/:status', ordersController.getOrdersByStatus); //Obtener pedidos por status
 router.get('/:id', ordersController.getOrderById);  //obtener pedido por id
 router.get('/', ordersController.getAllOrders); // Ruta para obtener todos los pedidos
-router.get('/status/:status', ordersController.getOrdersByStatus); //Obtener pedidos por status
 router.put('/:id', ordersController.cancelOrder);
 
 export default router;
